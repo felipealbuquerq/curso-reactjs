d2ab962e34ddcb9e14ab5bec220f5198321619b4
felipedealmeida <f.albuquerquedealmeida@hotmail.com>
2019-02-27 01:46:52 -0300

    Codigo Inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Header from './components/Header';
+
+import "./styles.css"
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+export default App;
